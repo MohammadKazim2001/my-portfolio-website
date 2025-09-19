74046860f2c7025b905e005b67faf32a653e3e3c
Kazim Amiri <mohammad.kazimamiri2001@gmail.com>
2025-09-19 21:45:12 +0430

    name has been updated in about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -15,18 +15,20 @@ function About() {
       <div className="container">
         <div className="bg-zinc-800/50 ring-inset ring-2 ring-zinc-50/10 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-lg md:max-w-[60ch]">
-            Welcome! I&apos;m Mohammad Kazim Amiri, a creative and passionate
-            Web Developer with hands-on experience in modern frontend
-            technologies — including React.js, TypeScript, and Tailwind CSS —
-            and backend development using Node.js, Express.js, MongoDB, and
-            PHP/Laravel. Eager to contribute to real-world projects, I bring
-            strong problem-solving skills, attention to detail, and a
-            collaborative mindset. Proficient in Git/GitHub and familiar with
-            Agile/Scrum workflows, I’m excited to learn from experienced
-            developers, grow through mentorship, and help build scalable,
-            user-focused applications. Fluent in English and Dari/Pashto, I’m
-            ready to bring energy, curiosity, and technical skill to your
-            development team — whether you’re a startup, agency, or enterprise.
+            Welcome! I&apos;m{" "}
+            <span className="text-sky-500">Mohammad Kazim Amiri</span>, a
+            creative and passionate Web Developer with hands-on experience in
+            modern frontend technologies — including React.js, TypeScript, and
+            Tailwind CSS — and backend development using Node.js, Express.js,
+            MongoDB, and PHP/Laravel. Eager to contribute to real-world
+            projects, I bring strong problem-solving skills, attention to
+            detail, and a collaborative mindset. Proficient in Git/GitHub and
+            familiar with Agile/Scrum workflows, I’m excited to learn from
+            experienced developers, grow through mentorship, and help build
+            scalable, user-focused applications. Fluent in English and
+            Dari/Pashto, I’m ready to bring energy, curiosity, and technical
+            skill to your development team — whether you’re a startup, agency,
+            or enterprise.
           </p>
           <div className="flex flex-wrap items-center gap-4 md:gap-7">
             {aboutItems.map(({ label, number }, key) => (
